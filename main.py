e2abcde65544670bf2930d5dd5888c3e82963f54
Jake <jake.pazzard@gmail.com>
2024-03-22 23:35:52 +0000

    Moved builds out and improvements for testing

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ from rich import print as prnt
 from rich.panel import Panel
 import pandas as pd
 
-def main(name: str = "", age: int = 0, lastname: str = "", headers: bool = False):
+def main():
     """
     This docsctring will be shown when running the --help flag
     """
-    print(f"Hello {name}, who is {age} years old!")
     
-    prnt(Panel("Hello, [red]World!"))
-    if lastname:
-        print(f"Also, your last name is {lastname}")
-    elif name:
-        print(f"Also, you have no last name, {name}")
-    elif headers:
+    prnt("[bold red]GSX Pro Profile Scanner[/bold red]")
+    prnt("Options:","\n- headers: Show the first 4 rows of the data file", "\n- clean: Clean the data file", "\n- leave: exit the program")
+    input = typer.prompt("What would you like to do?")
+    if input == "headers":
         airport_data = pd.read_csv("airports.csv")
+        airport_data = clean_data(airport_data)
         print(airport_data.head(4))
+        main()
+    elif input == "clean":
+        airport_data = pd.read_csv("airports.csv")
+        print("Cleaning data")
+        airport_data = clean_data(airport_data)
+        print("Data cleaned")
+        main()
+    elif input == "leave":
+        print("Goodbye")
+        raise typer.Exit()
+       
+def clean_data(data):
+    return data
 
-
-
-
-if __name__ == "__main__":
-    typer.run(main)
+typer.run(main)
