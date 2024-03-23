28cd75533dedf7925f103e62493727bcc093fcfa
Jake <jake.pazzard@gmail.com>
2024-03-23 01:44:37 +0000

    Basic Menu Creation

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import typer
 from rich import print as prnt
 from rich.panel import Panel
+from rich.markdown import Markdown
+from rich.table import Table
 import pandas as pd
 
 def main():
-    """
-    This docsctring will be shown when running the --help flag
-    """
+
+    commands_table = Table("Command", "Trigger", "Description", style="bold yellow")
+    commands_table.add_row("Scan Folder","scan", "Scan the specified profiles folder for GSX profiles")
+    commands_table.add_row("Settings","settings", "View and edit the settings for this program")
+    commands_table.add_row("Help","help", "View the help menu")
+    commands_table.add_row("Exit","exit", "Exit the program")
+
+    prnt(
+        Panel(Markdown("# GSX Pro Profile Scanner"), title="Welcome", style="bold green"),
+        Panel(commands_table, title="Commands", style="bold yellow", title_align="left"),
+        Panel(Markdown(
+"""
+1. Use the 'commands' box to view all available commands
+2. To run a command, type the trigger word in the input box
+"""), title="Instructions", style="bold red", title_align="left")
+
+)
     
-    prnt("[bold red]GSX Pro Profile Scanner[/bold red]")
-    prnt("Options:","\n- headers: Show the first 4 rows of the data file", "\n- clean: Clean the data file", "\n- leave: exit the program")
-    input = typer.prompt("What would you like to do?")
-    if input == "headers":
-        airport_data = pd.read_csv("airports.csv")
-        airport_data = clean_data(airport_data)
-        print(airport_data.head(4))
+    input = typer.prompt("Action")
+
+
+    if input == "scan":
+        scan()
         main()
-    elif input == "clean":
-        airport_data = pd.read_csv("airports.csv")
-        print("Cleaning data")
-        airport_data = clean_data(airport_data)
-        print("Data cleaned")
+    elif input == "settings":
         main()
-    elif input == "leave":
+    elif input == "help":
+        main()
+    elif input == "exit":
         print("Goodbye")
         raise typer.Exit()
-       
-def clean_data(data):
-    return data
+
+def scan():
+    pass
+
+def settings():
+    pass
+
+def help():
+    pass
+
+
 
 typer.run(main)
